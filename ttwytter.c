0e139f0b919553679e0fef317e6f9727b52ddb41
Ásgeir Berg Matthíasson <asgeir@asgeirberg.org>
2014-06-14 15:37:15 +0000

    Initial commit

---
+++ ttwytter.c
@@ -0,0 +1,82 @@
+#include "ttwytter.h"
+
+int main (int argc, char **argv)
+{
+  response.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+  response.size = 0;    /* no data at this point */
+
+  init_libcurl(argc, argv);  /* initalize the keys */
+  curl_global_init(CURL_GLOBAL_ALL);
+  CURL *curl = curl_easy_init();
+  
+  if (parse_arguments(argc, argv) != 0)
+  {
+    return 1;
+  }
+
+  if (get_tweet_flag == 1)
+  {
+    get_data(curl); /* get the tweet */
+  }  
+
+  curl_global_cleanup(); /* do the cleaning up for libcurl */
+    
+  return 0;
+}
+
+int parse_arguments(int argc, char **argv)
+{
+  int c;
+
+  while ((c = getopt(argc, argv, "c:qf:u:")) != -1)
+  {
+    switch (c)
+    {
+      case 'q': 
+        quiet_flag = 1; 
+
+        break;
+      case 'u':
+        get_tweet_flag = 1;
+
+        if (strlen(optarg) < 16) //check the length of the input. Twitter only allows 15 chars.
+        {
+          screen_name = optarg;
+        }
+        else
+        {
+          output(stderr, "%s is too long. Only 15 characters are allowed.\n", optarg);
+          return 1;
+        }
+
+        break;
+      case 'f':
+        filename = optarg;
+
+        break;
+      case 'c': //at present -c also accept strings as arguments. This should be restricted to integers. (The type of the argument is always a string)
+        if (strlen(optarg) < 5)
+        {
+          count = malloc(sizeof(char) * 4);
+          strcpy(count, optarg);
+        }
+        else
+        {
+          printf("-c only accepts arguments up to 9999.\n");
+
+          return 1;
+        }
+
+      break;
+      case '?':
+        if (optopt == c)
+        {
+          output(stderr, "Option -%c requires an argument.\n", optopt);
+        }
+      return 1;
+    }
+  }
+  return 0;
+}
+
+
---
+++ ttwytter.h
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <oauth.h>
+#include <curl/curl.h>
+#include <stdarg.h>
+#include "cJSON.h"
+
+#define FORMAT_SIZE 5000
+
+//prototypes
+int init_libcurl(int argc,  char **argv); //this just initilizes some things needed for libcurl
+int get_data();
+int parse_jason(char *response); //parses the output from libcurl.
+void output(FILE *stream, const char *output, ...);
+size_t static write_to_memory(void *response, size_t size, size_t nmemb, void *userp);
+int parse_arguments(int argc, char **argv);
+
+char* consumer_key = NULL;
+char* consumer_secret = NULL;
+char* user_token = NULL;
+char* user_secret = NULL;
+
+//command line flags
+unsigned short int user_flag = 0;
+unsigned short int get_tweet_flag = 0;
+unsigned short int post_tweet_flag = 0;
+unsigned short int quiet_flag = 0; //silences errors to the terminal. That which is sent to stdout still is output.
+char *filename;
+
+//this is used by the callback function to reserve memory for the response.
+struct Buffer {
+  char *memory;
+  size_t size;
+};
+
+struct Buffer response;
+
+//function arguments after parsing
+char *screen_name;
+char *count; //one is the default
+
+CURL *curl;
+
+FILE *f = NULL;
+
+int init_libcurl(int argc, char **argv)
+{
+  consumer_key = "scgvVzQDrwKfbZUZtepaZOvdz";
+  consumer_secret = "Ou0NfStxJxSjR0y1i0GuZHQUBQRd3e7yQ8LQrd0WROSrxTfhtc";
+  user_token = "1128798817-TdkOFKyFFuhUmu1SQNu58dokhm3ZdrTSHoYfcUq";
+  user_secret = "XQCTY1aro7vx3Hb14SLgLewYoiSyofDxqRFAlZAQou2qa";
+
+  return 0;
+}
+
+int get_data(CURL *curl)
+{
+
+  char *url = malloc(sizeof(char) * 256);
+
+  if (get_tweet_flag) /* build the url with a query string to get tweet*/
+  {
+    strcpy(url, "https://api.twitter.com/1.1/statuses/user_timeline.json");
+    strcat(url, "?screen_name=");
+    strcat(url, screen_name);
+    strcat(url, "&count=");
+    
+    if (count == NULL) /* if count is not set by the user, 1 is put as default. */
+    {
+      count = malloc(sizeof(char) * 2);
+      strcpy(count, "1");
+    }
+
+    strcat(url, count);
+
+    free(count); /* this also frees the count set in the parse_arguments-function */
+
+  }
+
+  char *signedurl = oauth_sign_url2(url, NULL, OA_HMAC, "GET", consumer_key, consumer_secret, user_token, user_secret);
+
+  curl_easy_setopt(curl, CURLOPT_URL, signedurl); /* URL we're connecting to, after being signed by oauthlib */
+  curl_easy_setopt(curl, CURLOPT_USERAGENT, "ttwytter/0.1"); // User agent we're going to use
+  curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1); // libcurl will now fail on an HTTP error (>=400)
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_memory); //setting a callback function to return the data
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&response); // passing the pointer to the response as the callback parameter
+    
+  int curlstatus = curl_easy_perform(curl); // Execute the request!
+
+  if (curlstatus != 0)
+    {
+      printf("curl_easy_perform terminated with status code %d\n", curlstatus);
+
+      return 1;
+    }
+  else
+    {
+      parse_jason(response.memory);  
+    }
+
+  curl_easy_cleanup(curl);
+
+  free(response.memory);
+  free(url);
+        
+  return 0;
+}
+
+int parse_jason(char *response)
+{
+
+  cJSON *array, *root, *child;
+  char *text, *screen_name;  
+
+  root = cJSON_Parse(response);
+  
+  if (!root) 
+  {
+    printf("Error before: [%s]\n",cJSON_GetErrorPtr());
+    return 1;
+  }
+  else
+  {
+    for (int i = 0; i < cJSON_GetArraySize(root); ++i)
+    {
+      array = cJSON_GetArrayItem(root, i); 
+      
+      if (!array)
+      {
+        printf("Error before: [%s]\n",cJSON_GetErrorPtr());
+        
+        return 1;
+      }
+
+      //actual parsing
+      text = cJSON_GetObjectItem(array, "text")->valuestring; //find the key we want
+      child = cJSON_GetObjectItem(array, "user");
+      screen_name = cJSON_GetObjectItem(child, "screen_name")->valuestring;
+  
+      output(stdout, "\"%s\" by %s\n", text, screen_name);
+      
+    }
+
+    cJSON_Delete(root); //dereference the pointer
+
+    return 0; 
+    } 
+}
+
+void output(FILE *stream, const char *format, ...) //outputs text. It's written in this way to allow the q-flag in a simple way.
+{
+    char msg[FORMAT_SIZE];
+    va_list ap;
+
+    va_start(ap, format);
+    vsnprintf(msg, sizeof(msg) - sizeof(char), format, ap);
+
+    if ((!quiet_flag && stream == stderr) || stream == stdout)
+    {
+      fprintf(stream, "%s\r\n", msg);
+      fflush(stream); 
+    }
+}
+
+static size_t write_to_memory(void *response, size_t size, size_t nmemb, void *userp) //this is basically from libcurl's getinmemory.c example
+{
+  size_t realsize = size * nmemb;
+  struct Buffer *mem = (struct Buffer *)userp;
+ 
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+
+  if (mem->memory == NULL)
+  {
+    /* out of memory! */ 
+    printf("Not enough memory (realloc returned NULL)\n");
+
+    return 0;
+  }
+ 
+  memcpy(&(mem->memory[mem->size]), response, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+ 
+  return realsize;
+}
